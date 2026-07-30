7a8e6fcd65b74b1216e44fd646bceb7acceb2889
andres mendez huera <andres.mendez52@uabc.edu.mx>
2026-07-29 18:06:27 -0700

    primer commit

---
+++ curp2.py
@@ -0,0 +1,70 @@
+
+import tkinter as tk
+
+
+
+ventana = tk.Tk()  # Crea la ventana
+ventana.title("Mi CURP")  # Título
+ventana.geometry("600x400")   # Tamaño
+
+tk.Label(ventana, text="porfavor ingresa tus datos para generar tu CURP").pack()  # Etiqueta
+
+
+#==================================
+#NOMBRE COMPLETO
+#==================================
+
+tk.Label(ventana, text="NOMBRE").place(x=360,y=40)  # Etiqueta
+
+
+tk.Label(ventana, text="Ingresa tu primer apellido:").place(x=10, y=90)
+apellidoPtk = tk.Entry(ventana)
+apellidoPtk.place(x=200, y=90)  # Campo de entrada
+
+tk.Label(ventana, text="Ingresa tu segundo apellido:").place(x=10, y=120)
+apellidoMtk = tk.Entry(ventana)
+apellidoMtk.place(x=200, y=120)  # Campo de entrada
+
+tk.Label(ventana, text="Ingresa tu nombre:").place(x=10, y=150)
+nombretk = tk.Entry(ventana)
+nombretk.place(x=200, y=150)  # Campo de entrada
+
+tk.Label(ventana, text="Ingresa tu genero:").place(x=10, y=180)
+generotk = tk.Entry(ventana)
+generotk.place(x=200, y=180)  # Campo de entrada
+
+#=================================
+#FECHA DE NACIMIENTO
+#==================================
+
+
+tk.Label(ventana, text="FECHA DE NACIMIENTO").place(x=360,y=160)  # Etiqueta
+
+
+#DIA
+tk.Label(ventana, text="Ingresa tu dia de nacimiento:").place(x=10, y=210)
+diatk = tk.Entry(ventana)
+diatk.place(x=200, y=210) # Campo de entrada
+#MES
+tk.Label(ventana, text="Ingresa tu mes de nacimiento:").place(x=10, y=240)
+mestk = tk.Entry(ventana)
+mestk.place(x=200, y=240) # Campo de entrada
+#AÑO
+tk.Label(ventana, text="Ingresa tu año de nacimiento:").place(x=10, y=270)
+aniotk = tk.Entry(ventana)
+aniotk.place(x=200, y=270) # Campo de entrada
+
+
+#curp = apellidoP[0:2] + apellidoM[0]+ nombre[0]+ dia + mes + anio + genero[0] + lugarNacimiento[0:3] + P14 + P15 + P16 + "00"
+
+
+def datos():
+    apellidoP = apellidoPtk.get()
+    apellidoM = apellidoMtk.get()
+    nombre = nombretk.get()
+    genero = generotk.get()
+    dia = dia.get()
+    mes = mes.get()
+    anio = anio.get()
+
+ventana.mainloop() 
